a4abfaaaa1a46d58c1fa992f6378c23bfee5fe66
Jeremy Chionglo <jpchionglo@gmail.com>
2020-07-21 13:59:25 -0500

    First Commit

---
+++ PizzaStore.Domain/Models/Pizza.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace PizzaStore.Domain.Models
+{
+
+  public class Pizza
+  {
+
+    public List<Topping> Toppings { get; set;}
+
+    public Crust Crust { get; set;}
+
+    public Size Size { get; set;}
+
+  }
+
+}
